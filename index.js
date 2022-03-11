d7a938815a341fd1670ba9bf1035d2380b87cbb7
SLEEPlESS <rosas_u@ymail.com>
2022-03-10 18:57:42 -0800

    turned cross roads into switch case and exported teammembers array

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Employee = require('./lib/Employee');
-const Engenieer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
+const render = require('./src/htmlTemplate');
 const teamMembersArr = [];
 
 // add manager function 
-const addManager = () => {
-    return inquirer.prompt([
+const addManager = async() => {
+     // destructured input items to be user input values
+    const {name, id, email, officeNumber} = await inquirer.prompt([
         {
             type: 'input',
             // we are using the same name given to the object property ex this.name <----- we are using the 'name' part
@@ -67,126 +68,188 @@ const addManager = () => {
             }
         }
     ])
-// using user input values for method props
-    .then(managerInput =>{
-        // destructured input items to be user input values
-        const {name, id, email, officeNumber} = managerInput;
-        // making new object with manager constructor
-        const manager = new Manager(name, id, email, officeNumber);
-        //Pushing new object in to temMembers array
-        teamMembersArr.push(manager);
-        console.log(teamMembersArr);
-        addTeamMember();
-
-    })
-
+        // using user input values for method prop
+            // making new object with manager constructor
+            const manager = new Manager(name, id, email, officeNumber);
+            //Pushing new object in to temMembers array
+            teamMembersArr.push(manager);
+            // console.log(teamMembersArr);
+            addTeamMember();
 };
 
 addManager();
 
 
 // cross roads function for user to add different employees "addTeamMember"
-const addTeamMember = () => {
-    return inquirer.prompt([
+const addTeamMember = async () => {
+    const { memberChoice } = await inquirer.prompt([
         {
             type: 'list',
-            name:'memberChoice',
+            name: 'memberChoice',
             message: 'What team member would you like to add?',
             choices: ['add Engineer', 'add Intern', 'quit and build']
-        },
-    ])
-    .then (choiceInput => {
-        console.log(choiceInput);
-        // to do
-        // determine what the user has selected
-
-        
-         if (choiceInput.memberChoice === 'add Engineer'){
-             // return addEngineer function call to inquire about engineer
-             return inquirer.prompt([
-                 {
-                     type: 'input',
-                     name: 'name',
-                     message:'Please enter your name (required)',
-                     validate: nameInput => {
-                         if(nameInput) {
-                             return true;
-                         } else {
-                             console.log('Please enter your name');
-                             return false;
-                         }
-                     }
-                 },
-
-                 {
-                     type: 'input',
-                     name: 'id', 
-                     message: 'Please enter your ID (required)',
-                     validate: idinput => {
-                         if(idinput){
-                             return true;
-                         } else {
-                             console.log('Please enter your ID');
-                             return false;
-                         }
-                     }
-
-                 },
-
-                 {
-                    type: 'input',
-                    name: 'email',
-                    message: 'Please enter your email(required)',
-                    validate: emailInput =>{
-                        if(emailInput) {
-                            return true;
-                        } else {
-                            console.log('Please enter your email');
-                            return false;
-                        }
-                    }
-                 },
-
-                 {
-                     type: 'input',
-                     name: 'github',
-                     mssage: 'Please enter your GitHub (required)',
-                     validate: githubInput => {
-                         if(githubInput){
-                             return true;
-                         } else {
-                             console.log('Please enter your GitHub');
-                             return false;
-                         }
-                     }
-                 }
-
-             ])
-            //  assigning user input values to object
-             .then (engineerInput => {
-                console.log('engineer added');
-                // deconstuctor for engineer properties 
-                const {name, id, email, github} = engineerInput;
-                // new object instance
-                const engineer = new Engineer(name, id, email, github);     
-                // push the new object to the array
-                teamMembersArr.push(engineer);
-                console.log(teamMembersArr);
-                // returns us to the selection menu for another teammember
-                addTeamMember();
-
-            })
-        
-        }  //  return inquirer prompt for intern in the next if statement
-         else if (addIntern){
-            // return addIntern function call to inquire about intern
-            
         }
-         
-    })
+    ])
+    switch (memberChoice) {
+        case 'add Engineer':
+            return addEngineer();
+        case 'add Intern':
+            return addIntern();
+        case 'quit and build':
+            return buildTeam();
 
+    }
 }
 
-// **addEngineer 
-// **addIntenr 
+
+const addEngineer = async () => {
+    const engineerInput = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter your name (required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter your ID (required)',
+            validate: idinput => {
+                if (idinput) {
+                    return true;
+                } else {
+                    console.log('Please enter your ID');
+                    return false;
+                }
+            }
+
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email(required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email');
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'github',
+            mssage: 'Please enter your GitHub (required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub');
+                    return false;
+                }
+            }
+        }
+
+    ])
+    console.log('Engineer added!');
+    // deconstuctor for engineer properties 
+    const { name, id, email, github } = engineerInput;
+    // new object instance
+    const engineer = new Engineer(name, id, email, github);
+    // push the new object to the array
+    teamMembersArr.push(engineer);
+    // console.log(engineer);
+    // console.log(teamMembersArr);
+    // returns us to the selection menu for another teammember
+    addTeamMember();
+}
+
+const addIntern = async() => {
+     const { name, id, email, school } = await
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter your name (required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter your ID (required)',
+            validate: idinput => {
+                if (idinput) {
+                    return true;
+                } else {
+                    console.log('Please enter your ID');
+                    return false;
+                }
+            }
+
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email(required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email');
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'school',
+            mssage: 'Please enter the school you graduated from(required)',
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your school');
+                    return false;
+                }
+            }
+        }
+
+    ]) 
+    console.log('Intern added!');
+            // deconstructed object props assigned to intern input values
+            // const { name, id, email, school } = internInput;
+            // intern object instance with assigned props
+            const intern = new Intern(name, id, email, school);
+            // push new team member in to array
+            teamMembersArr.push(intern);
+            console.log(teamMembersArr);
+            return addTeamMember();
+}
+
+
 // **Build team 
+const buildTeam = () => {
+    // maDE index file and then passed in data 
+    fs.writeFileSync('index.html', render(teamMembersArr))
+}
--- src/htmlTemplate.js
+++ src/htmlTemplate.js
@@ -1,4 +1,52 @@
 //   take the array and buld TEMPLATE
 
+function createCards (team) {
+    const html = [];
+    for (const teamMember of team){
+        // getrole comes from object method
+        switch(teamMember.getRole()) {
+            case 'Manager':
+                html.push(generateManagerCard(teamMember))
+            case 'Engineer':
+                html.push(generateEngineerCard(teamMember))
+            case 'Intern':
+                html.push(generateInternCard(teamMember))
+        }
+    }
+    // ,join converts arr in to string
+    return html.join('');
+}
 
-module.exports = generateHtml => {};
+function generateManagerCard(manager) {
+    return `
+    <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${manager.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${manager.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+        </ul>
+    </div>
+</div>
+    `
+}
+
+
+module.exports = team => { return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    ${createCards(team)}
+</body>
+</html>
+`};
